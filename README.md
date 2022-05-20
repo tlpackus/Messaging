9fd0a279bd9e58a8a6195557d3cc2fde883cd5fe
devoncowell <104590223+devoncowell@users.noreply.github.com>
2022-05-20 11:39:21 -0700

    Add READ ME and outbound text file with working draft code.

---
+++ README.md
@@ -0,0 +1,33 @@
+# Messaging
+
+#### This application shows how much a number of USD is in other currencies.
+
+#### By Theron Packus & Devon Cowell
+
+## Technologies Used
+
+* VS Code
+* HTML5/CSS
+* JavaScript ES6
+
+## Description
+
+
+
+## Setup/Installation Requirements
+
+* 
+
+## Commit Aligned Performance Documentation
+
+*_runtime_meta
+memory_used_mb
+57
+duration_ms
+31_
+
+## Known Bugs
+
+*
+
+## License
---
+++ outboundText.js
@@ -0,0 +1,82 @@
+function parentNameHandler(newParentId) {
+let parentNameFix = newParentId;
+switch (parentNameFix) {
+    case ('21'):
+    return 'leads';
+    break;
+    case ('3'):
+    return 'people';
+    break;
+    case ('7'):
+    return 'opportunities';
+    break;
+    default:
+    return 'NoMatchFoundError';
+    };
+};
+
+function removeNonIntegers (dirtyNumberString) {
+let cleanedCustomerPhone = "";
+for (let i = 0; i < dirtyNumberString.length; i++) {
+    if (!isNaN(dirtyNumberString[i])) {
+    cleanedCustomerPhone += dirtyNumberString[i];
+    } else {
+    cleanedCustomerPhone += "";
+    }
+    
+}
+console.log(cleanedCustomerPhone);
+return cleanedCustomerPhone;
+}
+
+function checkOne(string){ 
+if (string[0] != "1") {
+    return ("1" + string);
+} else {
+    return string;
+}
+}
+
+function lengthCheck(number) {
+let correctLengthNumber = "";
+if (number.length > 11) {
+    return "the customer number entered is too long";
+} else if (number.length < 11) {
+    return "the customer number entered is too short";
+} else {
+    correctLengthNumber = number;
+}
+return correctLengthNumber;
+}
+
+function cleanNumbers(numbers){
+let numberArray = numbers.split(",");
+let selectedNumberString = numberArray[0];
+let spaceRemovedString = selectedNumberString.split(" ").join("");
+let integerConfirmedString = removeNonIntegers(spaceRemovedString);
+let firstNumberConfirmedInString = checkOne(integerConfirmedString);
+let finalCustomerNumber = lengthCheck(firstNumberConfirmedInString);
+return finalCustomerNumber;
+};
+
+function newMessageHandler(newMessageData) {
+let ownerNameLowCase = newMessageData.owner.toLowerCase();
+let ownerNameReadyToCompare = ownerNameLowCase.split(" ").join("");
+let keyArray = Object.keys(newMessageData);
+let ownerIndexValue = keyArray.indexOf(ownerNameReadyToCompare);
+let valueArray = Object.values(newMessageData);
+let ownerConfirmedNumber = valueArray.splice(ownerIndexValue, 1)
+let confirmedParentName = parentNameHandler(newMessageData.parentTypeIdentifier);
+let confirmedCustomerPhone = cleanNumbers(newMessageData.numbers);
+console.log(confirmedCustomerPhone);
+output = {
+    ownerName: newMessageData.owner,
+    ownerNumber: ownerConfirmedNumber.toString(),
+    parentName: confirmedParentName,
+    customerNumber: confirmedCustomerPhone
+};
+return output;
+};
+
+
+newMessageHandler(inputData);
